5a8bcc06998add7d78f3bceacd9549862acdca4c
DonutsLover <dassodenise@hotmail.com>
2017-09-28 00:39:39 -0300

    Agrego bibliotecas y funciones de clientes y servidores

--- Compartida/clientes.c
+++ Compartida/clientes.c
@@ -5,4 +5,33 @@
  *      Author: utnso
  */
 
+#include "clientes.h"
+
+//Funcion que usa Master, quien sabe a que host y port conectarse (porque se lo dijo Yama)
+socket_t conectarse_a_worker(char* host, char*port){
+	socket_t server_socket;
+	server_socket = conectar_cliente(host, port);
+	return server_socket;
+}
+
+//Funcion que usa Master para conectarse a Yama
+socket_t conectarse_a_yama(char* host, char*port){
+	socket_t server_socket;
+	server_socket = conectar_cliente(host, port);
+	return server_socket;
+}
+
+//Funcion que usa Yama para conectarse a FileSystem
+socket_t conectarse_a_fsystem(char* host, char*port){
+	socket_t server_socket;
+	server_socket = conectar_cliente(host, port);
+	return server_socket;
+}
+
+//Funcion que usa FileSystem para conectarse a DataNode
+socket_t conectarse_a_datanode(char* host, char*port){
+	socket_t server_socket;
+	server_socket = conectar_cliente(host, port);
+	return server_socket;
+}
 
--- Compartida/clientes.h
+++ Compartida/clientes.h
@@ -10,5 +10,20 @@
 
 #include "sockets.h"
 
+//Funcion que usa Master, quien sabe a que host y port conectarse
+
+socket_t conectarse_a_worker(char* host, char*port);
+
+//Funcion que usa Master para conectarse a Yama
+
+socket_t conectarse_a_yama(char* host, char*port);
+
+//Funcion que usa Yama para conectarse a FileSystem
+
+socket_t conectarse_a_fsystem(char* host, char*port);
+
+//Funcion que usa FileSystem para conectarse a DataNode
+
+socket_t conectarse_a_datanode(char* host, char*port);
 
 #endif /* CLIENTES_H_ */
