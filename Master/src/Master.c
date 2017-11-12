020212970a071d133aaa3b18f09fcc86d8c7652d
JereRamirez <jeremias.ramirez91@gmail.com>
2017-11-12 20:26:29 -0300

    Prototipos de las funciones del Master, ver conexiones por socket

--- Master/src/Master.c
+++ Master/src/Master.c
@@ -14,8 +14,41 @@
 #include <Shared/sockets.h>
 #include <Shared/protocolosNew.h>
 
+char etapaTransformacion(char IP, char port, int block, char newFileName){
+	char resultado = 'FAIL';
+//ACA ME CONECTO CON EL WORKER Y LE MANDO: el TRANSFORMADOR, bloque en el cual debe hacer la transformacion, bytes ocupados en el bloque y newFileName
+	//recibo el resultado del Worker y lo meto en resultado para devolverselo a YAMA
+
+return resultado;
+}
+
+char etapaReduccionLocal(char IP, char port, char tempFiles, char newFileName){
+	char resultado = 'FAIL';
+//ACA ME CONECTO CON EL WORKER Y LE MANDO: el REDUCTOR, lista de archivos temporales del Nodo newFileName
+	//recibo el resultado del Worker y lo meto en resultado para devolverselo a YAMA
+//por cada hilo que creo, voy llevando un contador que se va restando cada vez que termina con exito un resultado
+return resultado;
+}
+
+char etapaReduccionGlobal(char IP, char port, char listaWorkers, char tempListFiles){
+	char resultado = 'FAIL';
+//ACA ME CONECTO CON EL WORKER ENCARGADO Y LE MANDO: el REDUCTOR, una lista/estructura de los workers con sus IPs y puertos, lista de archivos temporales de Reduccion Local
+	//recibo el resultado del Worker y lo meto en resultado para devolverselo a YAMA
+
+return resultado;
+}
+
+char etapaAlmacenadoFinal(char IP, char port, char ReduGlobalFileName){
+	char resultado = 'FAIL';
+//ACA ME CONECTO CON EL WORKER ENCARGADO Y SOLICITo: que se conecte al filesystem y le envie el contenido del archivo de Reduccion Global y el nombre y ruta que este debera tener.
+	//recibo el resultado del Worker y lo meto en resultado para devolverselo a YAMA
+
+return resultado;
+}
+
 char host[20];
 char port[20];
+
 
 int main(int argc, char *argv[]) {
 	/********************************************/
