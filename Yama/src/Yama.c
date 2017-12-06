eeb2e3fb70ab06f01347def1c6f6a6510e8abb20
Ezequiel Iriso <ezequieliriso@gmail.com>
2017-12-05 21:51:57 -0300

    UN PEqueño Update

--- Yama/src/Yama.c
+++ Yama/src/Yama.c
@@ -5,6 +5,21 @@
 #include "libConexion.h"
 #include <commons/config.h>
 #include <commons/log.h>
+enum {
+    SENTINELA, //este estado jamas se usa pero nos indica si una estructura esta vacia
+    EN_ESPERA = 1,
+    EN_PROCESO,
+    FINALIZADO_OK,
+    ERROR=-1
+};
+
+enum {
+    TRANSFORMACION = 1,
+    REDUCCION_LOCAL,
+    REDUCCION_GLOBAL,
+    ALMACENAMIENTO,
+
+};
 #include "yamaLib.h"
 #include <commons/collections/list.h>
 
@@ -18,31 +33,6 @@ pthread_mutex_t listaMaster_m;
 pthread_mutex_t filesystem_m;
 pthread_mutex_t planificador;
 
-enum {
-	SENTINELA, //este estado jamas se usa pero nos indica si una estructura esta vacia
-	EN_ESPERA = 1,
-	EN_PROCESO,
-	FINALIZADO_OK,
-	ERROR=-1
-};
-
-enum {
-	TRANSFORMACION = 1,
-	REDUCCION_LOCAL,
-	REDUCCION_GLOBAL,
-	ALMACENAMIENTO,
-
-};
-
-typedef struct {
-	int32_t job;
-	int32_t master;
-	int32_t	nodo;
-	int32_t bloque;
-	int32_t estado;
-	int32_t etapa;
-	char* archTemp;
-}tablaEst;
 
 
 void levantarConfig(int32_t *puertoServerYama,char* ipFileSystem, int32_t *puertoFileSystem, int32_t *handshakeFileSystem,int32_t* handshakeMasters)
@@ -67,7 +57,7 @@ void levantarLog()
 }
 
 char* AsignarNombreUnico(int semilla){
-	return "nombreunico";
+	return "nombreunico";//TROLL
 }
 
 void guardarNodosDisponibles(t_list * nodosFilesystem, t_list * nodosLocales)
@@ -81,15 +71,46 @@ void manejaMasterPlanificador(int32_t socketMaster){
 
 	char* archivoYamafs = malloc(data);
 	bytesRecibidos= recibir(socketMaster,archivoYamafs,(data),logger);//RECIBO NOMBRE ARCHIVO FS//chek conexion?
-	data=RQ_ARCH_FS;
-	pthread_mutex_lock(&filesystem_m);
+    pthread_mutex_lock(&filesystem_m);
+    t_list* Nodos= arrayNodos(socketFs,logger);
+    pthread_mutex_unlock(&filesystem_m);
+
+    data=RQ_ARCH_FS;
+    pthread_mutex_lock(&filesystem_m);
 	enviar(socketFs,&data,sizeof(int32_t),logger);
 	data=strlen(archivoYamafs);
 	enviar(socketFs,&data,sizeof(int32_t),logger);
 	enviar(socketFs,archivoYamafs,data,logger);
 	bytesRecibidos= recibir(socketMaster,&data,sizeof(data),logger);//chek conexion?
-	BlockFs* bloques=arrayBlockFs(data,logger);
+	BlockFs* bloques=arrayBlockFs(socketFs,data,logger);
 	pthread_mutex_unlock(&filesystem_m);
+    //INICIO PRE PLANIFICACION
+
+    //FIN PRE PLANIFICACION
+    //ENVIO MASTER
+
+    //ESPERO ESTADOS ESPERO RECEIVE POR CANTIDAD DE WORKERS // SI FALLA REPLANIFICO ALGUNO O TERMINO SI NO SE PUEDE
+
+    //PREPLANIFICO REDU LOCAL
+    //ORIGINO NOMBRES RANDM de FTEmp Y MANDO INSTRUCCIONES A LOS NODOS PAR LAS RECUDDIONES LOCALES
+    //nodo, archivosTemp en NOdo, ArchivoRedLocal
+    //FIN PREPLANIFIC REDU LOCAL
+    //ENVIO MASTER
+
+    //Espero Estado, si falla algo, a la goma
+
+    //PREPLANIFICO REDU GLOBAL
+    //ORIGINO NOMBRES RANDM de FTEmp Y MANDO INSTRUCCIONES A LOS NODOS PAR LAS RECUDDIONES GLOBALES
+    //nodo, ARCHIVOLOCALNODO, NODO AL QUE CONECTARTE(IP Y PUERTO), SI SOY RECEPTOR
+
+    //FIN PREPLANIFIC REDU GLOBAL
+    //ENVIO MASTER
+
+    //ESPERO ESTADOS SI EXPPLOTA JUDIO MENOS -- PUKY JODETE-- ADRO ES TURKO KBOOM
+
+    //LOAD TO FS
+    //MANDO A NODO FINAL A GUARDAR FILE DE REDUCCION GLOBAL CON EL NOMBRE ESPECIFICO en FS
+    //ESPERO RESOLUCION Y CIERRO CONEXION CON MASTER
 
 
 }
@@ -116,22 +137,25 @@ int main(int argc, char *argv[]) {
 	/* lo primero que hago es conectarme con filesystem y obtener la lista completa de nodos a usar*/
     socketFs= cliente(ip_fs, puerto_fs, hs_fs, logger);
 	int32_t socket_server = servidor(puerto,99,logger);
-	int32_t *masterAux;
-	while(true){
-		masterAux = malloc(sizeof(int32_t));
+	MasterPTH aux;
+    aux.state = true;
+    while(true){
+		aux.socketMaster = malloc(sizeof(int32_t));
 		log_info(logger,"ESPERANDO CONEXION MASTER");
-		*masterAux = aceptarCliente(socket_server,hs_ms,logger);
+		*aux.socketMaster = aceptarCliente(socket_server,hs_ms,logger);
 
-		if(*masterAux>0){
-			log_info(logger,"NUEVO MASTER CONECTADO FD=%d",*masterAux);
+		if(*aux.socketMaster>0){
+			log_info(logger,"NUEVO MASTER CONECTADO FD=%d",*aux.socketMaster);
 			pthread_mutex_lock(&listaMaster_m);
-			list_add(masters,masterAux);
+			list_add(masters,&aux);
+            aux.hilo=malloc(sizeof(pthread_t));
+            pthread_create(aux.hilo,NULL,manejaMasterPlanificador,aux.socketMaster);
 			pthread_mutex_unlock(&listaMaster_m);
-			masterAux=NULL;
+			aux.hilo=aux.socketMaster=NULL;
 		}
 		else{
 			log_error(logger,"NO ES MAESTRO, es solo un ENTRENADOR");
-			free(masterAux);
+			free(aux.socketMaster);
 		}
 	}
 
--- Yama/src/yamaLib.h
+++ Yama/src/yamaLib.h
@@ -1,31 +1,153 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include "libConexion.h"
 #include <commons/config.h>
 #include <commons/log.h>
 #include <commons/collections/list.h>
 #define RQ_ARCH_FS 4567
 #define RQ_BLCK_FS 3446
+#define RQ_WRKS_FS 3452
+
+typedef struct {
+    int32_t* socketMaster;
+    pthread_t* hilo;
+    bool state;
+}MasterPTH;
+
+typedef struct {
+    int32_t job;
+    int32_t master;
+    char*	nodo;
+    int32_t bloque;
+    int32_t estado;
+    int32_t etapa;
+    char* archTemp;
+}tablaEst;
+
+typedef struct {
+    int32_t id;
+    int32_t next_id;
+    char*   nombre;
+    int32_t puertoWorker;
+    char*   ipWorker;
+    int32_t estado;
+    int32_t disponibilidad;
+    int32_t clock;
+}NodoFs;
+
 
 typedef struct{ //FILESYSTEM{
         int32_t bloque_cp0;
-        int32_t nodo_cp0;
+        char*   nodo_cp0;
         int32_t bloque_cp1;
-        int32_t nodo_cp1;
+        char*   nodo_cp1;
         int32_t tamanio;
+        int32_t copyInUse;
 }BlockFs;
 
+bool listaContineStr(t_list* lista,char* valor){
+    int i;
+    tablaEst* asign;
+    for(i=0;i<list_size(lista);i++){
+        asign = list_get(lista,i);
+        if(!strcmp(asign->nodo,valor))
+            return true;
+    }
+    return false;
+}
 
-BlockFs* arrayBlockFs(int size,t_log* logger){
+
+int32_t cargaNodo(NodoFs* node,t_list* tabla){
+    int i,carga=0;
+    for(i=0;i<list_size(tabla);i++){
+        tablaEst* aux=list_get(tabla,i);
+        if(!strcmp(aux->nodo,node->nombre))
+            carga++;
+    }
+    return carga;
+}
+
+int32_t wlMax(t_list* tabla){
+    t_list* nodosTabla = list_create();
+    int i;
+    char *asign;
+    for(i=0;i<list_size(tabla);i++){
+        tablaEst* aux=list_get(tabla,i);
+        if(!listaContineStr(nodosTabla,aux->nodo)){
+            asign=malloc(sizeof(strlen(aux->nodo)));
+            strcpy(asign,aux->nodo);
+            list_add(nodosTabla,asign);
+            asign = NULL;
+        }
+    }
+    int32_t max=0;
+    for(i=0;i<list_size(nodosTabla);i++){
+        tablaEst* aux=list_get(tabla,i);
+
+        if(cargaNodo(aux->nodo,tabla) > max){
+            max=cargaNodo(aux->nodo,tabla);
+        }
+    }
+    list_destroy(nodosTabla);
+    return max;
+}
+
+int32_t pwl(NodoFs* nodo,t_list* listaEstados){
+    return wlMax(listaEstados) - cargaNodo(nodo,listaEstados);
+}
+
+u_int32_t availability(NodoFs* nodo,int base,t_list* listaEstados,char* algoritm){
+
+    if(!strcmp("WCLOCK",algoritm))
+        return base + pwl(nodo,listaEstados);
+    if(!strcmp("CLOCK",algoritm))
+        return base;
+    return 0;
+}
+
+void recibeString(int32_t socket, char* aux,t_log* logger){
+    int32_t data;
+    recibir(socket,&data,sizeof(int32_t),logger);
+    aux = malloc(data+1);
+    recibir(socket,aux,data,logger);
+    aux[data]= '\0';
+}
+
+t_list* arrayNodos(int32_t socketFs,t_log *logger){
+    int32_t bytes,i,size,msg=RQ_WRKS_FS;
+    enviar(socketFs,&msg,sizeof(int32_t),logger);
+    bytes = recibir(socketFs,&size,sizeof(int32_t),logger);
+    if(bytes<=0)
+        return NULL;
+    t_list* nodos = list_create();
+    NodoFs* aux;
+    for(i=0;i<size;i++){
+        aux= malloc(sizeof(NodoFs));
+        recibeString(socketFs,(aux->nombre),logger);
+        recibir(socketFs,&(aux->puertoWorker),sizeof(int32_t),logger);
+        recibeString(socketFs,(aux->ipWorker),logger);
+        aux->estado =1;
+        aux->clock=0;
+        aux->next_id=-1;
+        aux->id=i;
+        aux->disponibilidad =0;
+        list_add(nodos,aux);
+        aux= NULL;
+    }
+    return aux;
+}
+
+BlockFs* arrayBlockFs(int32_t socketFs,int size,t_log* logger){
     BlockFs *archivo = malloc(sizeof(BlockFs)*size);
     int i;
     for(i=0;i<size;i++){
         recibir(socketFs,&archivo[i].bloque_cp0,sizeof(int32_t),logger);
-        recibir(socketFs,&archivo[i].nodo_cp0,sizeof(int32_t),logger);
+        recibeString(socketFs,archivo[i].nodo_cp0,logger);
         recibir(socketFs,&archivo[i].bloque_cp1,sizeof(int32_t),logger);
-        recibir(socketFs,&archivo[i].nodo_cp1,sizeof(int32_t),logger);
+        recibeString(socketFs,archivo[i].nodo_cp1,logger);
         recibir(socketFs,&archivo[i].tamanio,sizeof(int32_t),logger);
+        archivo[i].copyInUse=-1;
     }
     return archivo;
 }
@@ -71,7 +193,7 @@ char* numToEstado(int estado){
     return retorno;
 }
 
-void printTablaEstador(void){
+void printTablaEstador(t_list* tabla){
     int i;
     tablaEst *aux;
     printf("|Job\t|Master\t|Nodo\t|Bloque\t|Etapa\t|Archivo Temporal\t\t|Estado|");
@@ -91,3 +213,31 @@ void printTablaEstador(void){
  *
  *
  */
+
+bool prioridadAvailability(void* elem1, void* elem2){
+    NodoFs* aux1 = (NodoFs*)elem1;
+    NodoFs* aux2 = (NodoFs*)elem2;
+    return aux1->disponibilidad>aux2->disponibilidad;
+}
+
+void availabilityNodos(int32_t base,char* alg,t_list* nodos, t_list* tablaEst){
+    NodoFs* aux;
+    int i;
+    for(i=0;i<list_size(nodos);i++){
+        aux=list_get(nodos,i);
+        aux->disponibilidad = availability(aux,base,tablaEst,alg);
+    }
+    list_sort(nodos,prioridadAvailability);
+    for(i=0;i<list_size(nodos);i++){
+        aux = list_get(nodos,i);
+        aux->id=i;
+        if(i==0)
+            aux->clock = 1;
+        else
+            aux->clock = 0;
+        if(i == list_size(nodos) -1)
+            aux->next_id = 0;
+        else
+            aux->next_id = i+1;
+    }
+}
